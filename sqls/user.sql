4daace493fa0e626dcffc96c6ebe5636796ab0ee
hokuanyu <hokuanyu1998@gmail.com>
2021-01-27 17:27:22 +0800

    Shinobi

--- sqls/user.sql
+++ sqls/user.sql
@@ -1,6 +1,6 @@
 GRANT ALL PRIVILEGES ON ccio.* TO 'admin'@'%';
 GRANT ALL PRIVILEGES ON ospos.* TO 'admin'@'%';
 CREATE USER 'repliuser'@'%' IDENTIFIED BY 'ucpe';
-GRANT REPLICATION SLAVE ON *.* TO 'repliuser’@’%';
+GRANT REPLICATION SLAVE ON *.* TO 'repliuser'@'%';
 FLUSH PRIVILEGES;
 
