df2a9b01714cfe754a64b58258785838974020c3
hokuanyu <hokuanyu1998@gmail.com>
2021-01-28 12:09:49 +0800

    sql

--- sqls/user.sql
+++ sqls/user.sql
@@ -1,3 +1,4 @@
+CREATE USER 'repliuser'@'%' IDENTIFIED BY 'ucpe';
 GRANT ALL PRIVILEGES ON ccio.* TO 'admin'@'%';
 GRANT ALL PRIVILEGES ON ospos.* TO 'admin'@'%';
 CREATE USER 'repliuser'@'%' IDENTIFIED BY 'ucpe';
