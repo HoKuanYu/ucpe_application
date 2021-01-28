e039c1e106a07e9075389d51d98064e4266d2893
hokuanyu <hokuanyu1998@gmail.com>
2021-01-28 12:15:53 +0800

    sql order & sql

--- sqls/user.sql
+++ sqls/user.sql
@@ -1,4 +1,4 @@
-CREATE USER 'repliuser'@'%' IDENTIFIED BY 'ucpe';
+CREATE USER 'admin'@'%' IDENTIFIED BY 'ucpe';
 GRANT ALL PRIVILEGES ON ccio.* TO 'admin'@'%';
 GRANT ALL PRIVILEGES ON ospos.* TO 'admin'@'%';
 CREATE USER 'repliuser'@'%' IDENTIFIED BY 'ucpe';
